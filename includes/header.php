86ddd5d44e7749a695a703c32b5dae5e470e0080
unknown <bduser@SIMON-PC.blackwellglobal.com>
2022-05-12 14:49:36 +0100

    Created and finished the login page

--- includes/header.php
+++ includes/header.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Finance Tracker Dashboard</title>
 
     <!-- stylesheet -->
-    <link rel="stylesheet" href="../public/style.css">
+    <link rel="stylesheet" href="./public/style.css">
+
+    
 </head>
 <body>
--- index.php
+++ index.php
@@ -3,15 +3,17 @@
 <!-- DIV -->
 <div class="container">
 
+
     <!-- Loginbox -->
     <div class="login-box">
-        <form action="" method="post">
+
+        <form action="" method="post" class="login-form">
             <!-- Username -->
-            <input type="text" name="username" id="password-input"><br>
+            <input type="text" name="username" id="username-input" placeholder="Username"><br>
             <!-- Password -->
-            <input type="password" name="password" id="password-input"><br>
+            <input type="password" name="password" id="password-input" placeholder="Password"><br>
             <!-- Submit Button -->
-            <button type="submit">Submit</button><br>
+            <button type="submit" id="submit-button">Submit</button><br>
         </form>
     </div>
 
