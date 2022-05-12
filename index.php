f99f7650845fd90328d55c3c8fdc47aa2bae4fde
unknown <bduser@SIMON-PC.blackwellglobal.com>
2022-05-12 13:02:18 +0100

    app structured defined

---
+++ index.php
@@ -0,0 +1,20 @@
+include './includes/header.php';
+
+<!-- DIV -->
+<div class="container">
+
+    <!-- Loginbox -->
+    <div class="login-box">
+        <form action="" method="post">
+            <!-- Username -->
+            <input type="text" name="username" id="password-input">
+            <!-- Password -->
+            <input type="password" name="password" id="password-input">
+            <!-- Submit Button -->
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+
+</div>
+
+include './includes/footer.php';
