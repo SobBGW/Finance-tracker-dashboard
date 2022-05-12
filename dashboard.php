f5222ff8651765bccf0a82271d11f9b0bd840a8b
unknown <bduser@SIMON-PC.blackwellglobal.com>
2022-05-12 16:18:23 +0100

    added logo and dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,18 @@
+<?php include './includes/header.php' ?>
+
+<?php 
+// If user NOT LOGGED IN redirect them to the Login Page
+// if(!isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] !== true){
+//     // CHANGE LOCATION OF REDIRECT
+//     header("location: welcome.php");
+//     exit;
+// }
+
+?>
+
+
+<div class="container">
+    
+</div>
+
+<?php './includes/footer' ?>
