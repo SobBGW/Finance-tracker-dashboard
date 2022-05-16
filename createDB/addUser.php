80dbe6b90c16aed5c5ca4b21649e9ec38a21c548
unknown <bduser@SIMON-PC.blackwellglobal.com>
2022-05-16 10:15:56 +0100

    Finished the addUser.php script

--- createDB/addUser.php
+++ createDB/addUser.php
@@ -7,5 +7,16 @@ if($conn -> connect_error) {
     die("connection failed:" . $conn -> connect_error);
 }
 
+// SQL Statement
 $sql = "INSERT INTO users (firstname, lastname, email, password, role, group) VALUES ('john', 'ham', 'john.ham@gmail.com', 'hejgajshjksadj', 'member', 'finance')";
+
+// Execute sql statement
+if($conn -> query($sql) === TRUE) {
+    echo "New record added successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+
+
 ?>
